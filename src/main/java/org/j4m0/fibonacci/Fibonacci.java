1f1d05df2f393ae05125a2467142b2988aad5abc
J4M0 <j4m0@outlook.com>
2013-08-21 15:28:34 -0500

    Se cambia nombre de clase y métodos estaticos por metodos de clase

--- src/main/java/org/j4m0/fibonacci/Fibonacci.java
+++ src/main/java/org/j4m0/fibonacci/Fibonacci.java
@@ -9,11 +9,11 @@ import java.io.InputStreamReader;
  * @author J4M0
  *
  */
-public class App {
+public class Fibonacci {
     
     public static BufferedReader ent = new BufferedReader(new InputStreamReader(System.in));
     
-    public static int fibonacciIterativo(int n, int arr[]){
+    public int iterativo(int n, int arr[]){
         for(int i = 0; i < n; i++){
             if(i < 2){
                 arr[i] = 1;
@@ -24,29 +24,29 @@ public class App {
         return arr[n-1];
     }
     
-    public static int fibonacciRecursivo(int n){
+    public int recursivo(int n){
          if(n < 2){
              return 1;
          } else {
-             return fibonacciRecursivo(n-1) + fibonacciRecursivo(n-2);
+             return recursivo(n-1) + recursivo(n-2);
          }
     }
     
     public static void main(String args[]) throws IOException{
-        
+        Fibonacci fibonacci = new Fibonacci();
         System.out.println("Dame el numero interacciones");
         int n = Integer.parseInt(ent.readLine());
         int arr[] = new int [n];
         System.out.println("Iterativo es:");
         long inicioIterativo, finIterativo;
         inicioIterativo = System.currentTimeMillis();
-        System.out.println("F(" + n + ")=" + fibonacciIterativo(n, arr));
+        System.out.println("F(" + n + ")=" + fibonacci.iterativo(n, arr));
         finIterativo = System.currentTimeMillis();
         System.out.println("Tiempo Iterativo es:  "+ ( finIterativo - inicioIterativo ) +" milliseconds");                
         System.out.println("Recursivo es:");              
         long inicioRecursivo, finRecursivo;
         inicioRecursivo = System.currentTimeMillis();
-        System.out.println("F(" + n + ")=" + fibonacciRecursivo(n-1));
+        System.out.println("F(" + n + ")=" + fibonacci.recursivo(n-1));
         finRecursivo = System.currentTimeMillis();
         System.out.println("Tiempo Recursivo es:  "+ ( finRecursivo - inicioRecursivo ) +" milliseconds");
     }
